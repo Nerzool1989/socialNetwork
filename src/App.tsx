9969fdce46af91cc901ec186fcb3fb3c45189cc7
Nerzool1989 <Nerzool1989@yandex.ru>
2020-04-12 20:22:41 +0300

    new project start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Footer} from './components/footer/Footer';
+import {Header} from './components/header/Header';
+import Navbar from './components/navigation/Navigation';
+import Profile from './components/profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App__wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav className='App__nav'>
+        <ul>
+          <li>Profiler</li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    )
+};
+
+export default Navigation;
